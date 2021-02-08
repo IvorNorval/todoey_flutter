f4436264ced8418ff74d4dff7e5c71a4a2aaec64
IvorNorval <67774913+IvorNorval@users.noreply.github.com>
2021-02-08 16:01:21 +0200

    Update landscape transition

--- lib/widgets/heading_widget.dart
+++ lib/widgets/heading_widget.dart
@@ -3,9 +3,43 @@ import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:todoey_flutter/services/hive_helper.dart';
 
-class Heading extends StatelessWidget {
+class Heading extends StatefulWidget {
+  @override
+  _HeadingState createState() => _HeadingState();
+}
+
+class _HeadingState extends State<Heading> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  final Tween<double> turnsTween = Tween<double>(
+    begin: 1,
+    end: 3,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      _controller.forward();
+    } else {
+      _controller.forward();
+    }
+
     return Consumer<HiveHelper>(
       builder: (context, hiveHelper, Widget child) {
         return Container(
@@ -14,16 +48,19 @@ class Heading extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Shimmer.fromColors(
-                baseColor: const Color(0xFFedddd4),
-                period: const Duration(milliseconds: 5000),
-                highlightColor: const Color(0xFF283d3b),
-                child: const Text(
-                  'ToDoey',
-                  style: TextStyle(
-                    color: Color(0xFFedddd4),
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.w700,
+              RotationTransition(
+                turns: turnsTween.animate(_controller),
+                child: Shimmer.fromColors(
+                  baseColor: const Color(0xFFedddd4),
+                  period: const Duration(milliseconds: 5000),
+                  highlightColor: const Color(0xFF283d3b),
+                  child: const Text(
+                    'ToDoey',
+                    style: TextStyle(
+                      color: Color(0xFFedddd4),
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
